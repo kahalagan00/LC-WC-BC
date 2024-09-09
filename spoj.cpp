80d80050559db5f1b3c458855d3016cac1499c53
kahalagan00 <privatelampas@gmail.com>
2024-09-08 20:41:52 -0700

    2637. Promise Time Limit Solved.

--- spoj.cpp
+++ spoj.cpp
@@ -1,8 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
+using db = double;
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+using str = string;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vll = vector<ll>;
+using vs = vector<str>;
+using vc = vector<char>;
 
-// Returns number of bits in the integer
-constexpr int pct(int x) { return __builtin_popcount(x); }
+// vector operations
+#define sz(x) int(size(x))
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+
+constexpr int pct(int x) { return __builtin_popcount(x); }	// Returns number of bits in the integer
+
+// Run and compile the program
+// g++ -o s spoj.cpp && ./s
 
 int max(int a, int b)
 {
@@ -12,32 +46,16 @@ int max(int a, int b)
 int solve()
 {
 	ios::sync_with_stdio(false), cin.tie(nullptr);
-	unordered_map<int, unordered_set<char>> r;	// row
-	unordered_map<int, unordered_set<char>> c;	// column
-	map<pair<int, int>, unordered_set<char>> b; // boxes
-
-	for (int i = 0; i < 9; ++i) // O(9)
-	{
-		for (int j = 0; j < 9; ++j) // O(9)
-		{
-			if (board[i][j] != '.')
-			{
-				if (r[i].count(board[i][j]) == 1 ||
-					c[j].count(board[i][j]) == 1 ||
-					b[{i / 3, j / 3}].count(board[i][j]) == 1)
-				{
-					return false;
-				}
-
-				// .insert() for unordered_set is O(1)
-				r[i].insert(board[i][j]);			   // O(1) * O(1)
-				c[j].insert(board[i][j]);			   // O(1) * O(1)
-				b[{i / 3, j / 3}].insert(board[i][j]); // O(log(9)) * O(1)
-			}
-		}
+	string s = "hello";
+	int total = 0;
+	for(int i = 0; i < s.size() - 1; ++i) {
+		total += abs((s[i] - '0') - (s[i + 1] - '0')); 
 	}
 
-	return true;
+	cout << total << '\n';
+	
+	return 0;
+	
 }
 
 int main()
